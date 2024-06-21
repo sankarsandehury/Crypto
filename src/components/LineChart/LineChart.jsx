10b963dba53aeee7e20270da8046834abf74e66d
sankarsandehury <sankarsadehury923@gmail.com>
2024-06-21 09:54:06 +0530

    initialize

---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import './LineChart.css'
+import Chart from 'react-google-charts'
+
+const LineChart = ({ historicalData }) => {
+  const [data, setData] = useState([["Date", "Prices"]])
+  useEffect(() => {
+    let datacopy = [["Date", "Prices"]]
+    if (historicalData.prices) {
+      historicalData.prices.map((item) => {
+        datacopy.push([`${new Date(item[0]).toLocaleDateString().slice(0, -5)}`, item[1]])
+      })
+      setData(datacopy);
+    }
+  }, [historicalData])
+  return (
+    <Chart
+      chartType='LineChart'
+      data={data}
+      height="100%"
+    />
+  )
+}
+
+export default LineChart
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const { setCurrency } = useContext(CoinContext)
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: 'usd', symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: 'eur', symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: 'eur', symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: 'usd', symbol: "$" });
+        break;
+      }
+    }
+  }
+
   return (
     <div className='navbar'>
-      <img src={logo} alt="" />
+      <Link to={'/'}>
+        <img src={logo} alt="" className='logo' />
+      </Link>
       <ul>
-        <li>Home</li>
+        <Link to={'/'}><li>Home</li></Link>
+        
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
